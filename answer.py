e998ccaa1ac42518e6e4f77c70f929ef3d2dc3f9
vnadipel <vnadipel@stevens.edu>
2024-01-24 17:02:24 -0500

    output values successfully printed

--- answer.py
+++ answer.py
@@ -1,17 +1,26 @@
+#assignment-1
+#Vaishnavi Nadipelly
 def hello_world():
     # Write a doc string below. Doc strings can be used to provide the user with a
     # description of what the function does.
     """
-
+        This function displays 'Hello, World!' on the console.
     """
 
     # Write a comment; comments are very useful to other programmers.
 
     # Write a Python statement to print Hello, World!
 
+    #output value printed
+    print('Hello, World!')
+
+hello_world()
+
 def add(a,b):
 
     # Write simple code to add variable a and b
-    output =
+    output = a + b
+    #output printed
 
+    print(output)
     return output
